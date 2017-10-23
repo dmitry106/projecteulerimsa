4a1b0ff9c32acdf1fb0d29cb9bc6ecb0c804d6be
dmitry106 <dimafeofanov@gmail.com>
2017-10-23 17:46:39 -0500

    adding solutions

---
+++ haskell/euler2.hs
@@ -0,0 +1,13 @@
+fib n = table !! n
+  where
+    table = 0 : 1 : zipWith (+) table (tail table)
+
+
+
+
+euler2 = sum[x | x<- takeWhile ( < 4000000) (map fib [1..]), even(x)]
+
+
+main = print euler2
+
+
