284b6f65d727180515d2c4c66eee5c77d8c0155d
dmitry106 <dimafeofanov@gmail.com>
2017-10-24 14:01:21 -0500

    Delete euler8.hs

--- haskell/euler8.hs
+++
@@ -1,9 +0,0 @@
-import Data.Char
-import Data.List
-main = do
-  str <- readFile "euler8text.txt"
-  print . maximum . map product
-        . foldr (zipWith (:)) (repeat [])
-        . take 13 . tails . map(fromIntegral . digitToInt)
-        . concat . lines $ str
-
