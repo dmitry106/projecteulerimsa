5f54abdd089d906331dda1c923d0f98e586504f8
dmitry106 <dimafeofanov@gmail.com>
2017-10-24 01:18:31 -0500

    solved 8

--- haskell/euler8.hs
+++ haskell/euler8.hs
@@ -1,2 +1,9 @@
---temporary ingress
+import Data.Char
+import Data.List
+main = do
+  str <- readFile "euler8text.txt"
+  print . maximum . map product
+        . foldr (zipWith (:)) (repeat [])
+        . take 13 . tails . map(fromIntegral . digitToInt)
+        . concat . lines $ str
 
