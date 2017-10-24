bfb7774fbb50b9e0e9eb2c9ed15af3fc69b55bb7
dmitry106 <dimafeofanov@gmail.com>
2017-10-24 00:59:21 -0500

    finished number 7

--- haskell/euler7.hs
+++ haskell/euler7.hs
@@ -1,2 +1,14 @@
---temp
---
+
+main = print euler7
+
+primes = 2 : filter ((==1) . length . primeFactors) [3,5..]
+
+primeFactors n = factor n primes
+  where
+    factor n (p:ps)
+      | p*p > n = [n]
+      | n `mod` p == 0 = p : factor (n `div` p) (p:ps)
+      | otherwise = factor n ps
+
+euler7 = primes !! 10000
+
